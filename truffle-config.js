28e5d4a8103156381226bb524025f3f7621270ef
Tauren Mills <tauren@orbslabs.com>
2020-12-14 10:33:46 -0800

    Setup ganache

--- truffle-config.js
+++ truffle-config.js
@@ -41,12 +41,12 @@ module.exports = {
     // options below to some value.
     //
 
-    // development: {
-    //   host: "127.0.0.1", // Localhost (default: none)
-    //   // port: 7545,
-    //   // port: 8545, // Standard Ethereum port (default: none)
-    //   network_id: "*", // Any network (default: none)
-    // },
+    development: {
+      host: "127.0.0.1", // Localhost (default: none)
+      // port: 7545,
+      port: 8545, // Standard Ethereum port (default: none)
+      network_id: "*", // Any network (default: none)
+    },
 
     // // Another network with more advanced options...
     // advanced: {
