edc5f6d2a248952e396a090e07bfdb3002a7d4f0
João Ritter <ritterjv@gmail.com>
2020-12-23 20:29:17 -0500

    Copied over from deploy-attempt branch, with a few touchups

---
+++ migrations/4_deploy_contracts.js
@@ -0,0 +1,12 @@
+const Fountain = artifacts.require("FountainV1");
+
+// Dynamically obtain DAI contract address based on deployment network (mainnet address is different than ropsten)
+const DAI = {
+  live: "0x6b175474e89094c44da98b954eedeac495271d0f",
+  ropsten: "0xad6d458402f60fd3bd25163575031acdce07538d",
+};
+
+module.exports = function (deployer, network, accounts) {
+  deployer.deploy(Fountain, DAI[network] || accounts[3]);
+};
+
