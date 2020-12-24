74945937ba16357d9824b39f01426fdb43862d44
João Ritter <ritterjv@gmail.com>
2020-12-23 20:06:36 -0500

    fixed DAI network for test and development networks

--- migrations/3_deploy_contracts.js
+++ migrations/3_deploy_contracts.js
@@ -4,9 +4,9 @@ const Fountain = artifacts.require("FountainV1");
 const DAI = {
   live: "0x6b175474e89094c44da98b954eedeac495271d0f",
   ropsten: "0xad6d458402f60fd3bd25163575031acdce07538d",
-  development: "...",
 };
 
 module.exports = function (deployer, network, accounts) {
-  deployer.deploy(Fountain, DAI[network]);
+  deployer.deploy(Fountain, DAI[network] || accounts[3]);
 };
+
