6023ef7e51bb4c2c7482a706c83def9d8089d174
João Ritter <ritterjv@gmail.com>
2021-01-03 18:31:45 -0500

    order

--- contracts/examples/YourContract.sol
+++ contracts/examples/YourContract.sol
@@ -5,7 +5,7 @@ import "../MoneyPoolOwner.sol";
 
 /// @dev This contract is an example of how you can use Fountain to fund your own project.
 contract YourContract is MoneyPoolOwner {
-    constructor(IERC20 _want, IFountain _fountain)
+    constructor(IFountain _fountain, IERC20 _want)
         public
         MoneyPoolOwner(_fountain, 10000, 60, _want)
     {}
