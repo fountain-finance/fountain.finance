adf77ed6249b1509bd9365400e860d6da5c20975
João Ritter <ritterjv@gmail.com>
2021-01-04 21:14:49 -0500

    ~

--- contracts/abstract/MoneyPoolOwner.sol
+++ contracts/abstract/MoneyPoolOwner.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "./interfaces/IFountain.sol";
+import "./../interfaces/IFountain.sol";
 
 abstract contract MoneyPoolOwner is Ownable {
     using SafeERC20 for IERC20;
--- contracts/examples/YourContract.sol
+++ contracts/examples/YourContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.6.0 <0.8.0;
 
-import "../MoneyPoolOwner.sol";
+import "../abstract/MoneyPoolOwner.sol";
 
 /// @dev This contract is an example of how you can use Fountain to fund your own project.
 contract YourContract is MoneyPoolOwner {
